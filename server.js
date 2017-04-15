6ee4a5e96a4f5497b6d5e1cb1eb0330adc11bb8b
EC2 Default User <ec2-user@ip-172-31-33-111.us-west-2.compute.internal>
2017-04-15 19:16:33 +0000

    Update server code

--- server.js
+++ server.js
@@ -4,35 +4,63 @@ var server = require('http').Server(app);//.createServer();
 var io = require('socket.io')(server);
 
 var players = [];
+var player_data = [];
+var messages_recieved = 0;
+var playback_data = [];
+//var next_player = 0;
 
-function Player (id) {
+function Player (id, col, data) {
+    this.color_id = col;
     this.id = id;
-    this.x = 0;
-    this.y = 0;
-    this.z = 0;
-    this.entity = null;
+    this.data_string = data;
 }
 
 io.on('connection', function(socket) {
-    console.log('Connected');
-    socket.on ('initialize', function () {
-            var idNum = players.length;
-            var newPlayer = new Player (idNum);
-            players.push (newPlayer);
+    console.log('Connected to ' + socket.id + ', players: ' + players.length);
+    socket.on ('initialize', function (msg) {
+	    var color_num = players.length;
 
-            socket.emit ('playerData', {id: idNum, players: players});
-            socket.broadcast.emit ('playerJoined', newPlayer);
+	    var new_player = new Player(socket.id, color_num, msg);
+
+	    socket.broadcast.emit('player_joined', {color_id: new_player.color_id, position:msg});
+            socket.emit('init_color', {color_id: new_player.color_id});
+	    socket.emit('player_data', {other_players: player_data});
+
+	    player_data.push({color_id: new_player.color_id, position:msg});
+	    players.push(new_player);
+	    //next_player = next_player + 1;
+
+	    console.log('New color_id: ' + players.length + ', message: ' + msg + ', players: ' + players.length);
+
     });
 
-    socket.on ('positionUpdate', function (data) {
-            players[data.id].x = data.x;
-            players[data.id].y = data.y;
-            players[data.id].z = data.z;
+    socket.on ('positionUpdate', function (msg) {
+	for (var i = 0; i<players.length; i++) {
+	    var player = players[i];
+	    console.log("player: " + player.id + " " + player.color_id);
+	    if (player.id == socket.id) {
+		console.log("got " + player.color_id + " msg ");
+		playback_data.push({id: player.color_id, data_string: msg});
+        	socket.broadcast.emit ('playerMoved', {id: player.color_id, data_string: msg});
+	    }
+	}
+	messages_recieved++;
+	if (messages_recieved >= players.length) {
+	    io.sockets.emit('playback_ready', playback_data);
+	}
+	
+    });
 
-        socket.broadcast.emit ('playerMoved', data);
+    socket.on('disconnect', function(){
+	console.log('user ' + socket.id + ' disconnected');
+	for (player in players) {
+	    if (player.id == socket.id) {
+		socket.broadcast.emit('player_left', player.color_id);
+	    }
+	}
     });
 });
 
-server.listen(80, function(){
+server.listen(8080, function(){
   console.log('listening on *:8080');
 });
